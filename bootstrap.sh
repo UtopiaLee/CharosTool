9f908f7b7202d21ec46d1185fc082f0132945b5f
charos <lijianwei580@gmail.com>
2026-06-06 16:08:40 +0800

    Fix: reset and clean before fetch to prevent merge conflicts

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,10 +15,10 @@ echo "Installing Linux Toolbox to $INSTALL_DIR..."
 if [ -d "$INSTALL_DIR" ]; then
     echo "Updating existing installation..."
     cd "$INSTALL_DIR"
-    sudo git fetch origin master
-    sudo git checkout -f master
-    sudo git reset --hard origin/master
+    sudo git reset --hard HEAD
     sudo git clean -fd
+    sudo git fetch origin master
+    sudo git reset --hard origin/master
 else
     echo "Cloning repository..."
     sudo git clone https://github.com/UtopiaLee/CharosTool.git "$INSTALL_DIR"
